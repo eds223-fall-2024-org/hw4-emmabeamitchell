323c35126b7ef47cff463cf84849cdfa34c0c204
emmabeamitchell <ebeatrice19@gmail.com>
2024-12-01 12:37:33 -0800

    final edits

--- r-script-function.R
+++ r-script-function.R
@@ -1,68 +1,81 @@
-files <- list.files(here("data", "data"), pattern = "average", full.names = TRUE)
+# List files to read in
+files <- list.files(here("data"), pattern = "average", full.names = TRUE)
 
-aqua_fun <- function (maxsst, minsst, maxdepth, mindepth) {
-  # read in rasters 
+# Make a function
+aqua_fun <- function (maxsst, minsst, maxdepth, mindepth, species) {
+  # Read in rasters 
   sst <- c(rast(files))
-  wc_eez <- st_read(here("data", "data", 'wc_regions_clean.shp'))
-  depth <- rast(here("data", "data", "depth.tif"))
-  # reproject
-  sst_stack_recrs <- project(sst, crs(depth))
-  wc_eez <- st_transform(wc_eez, crs(depth))
-  
-  # find mean sst
+  wc_eez <- st_read(here("data", 'wc_regions_clean.shp'))
+  depth <- rast(here("data", "depth.tif"))
+  #Find mean sst
   mean_sst <- app(sst_stack, mean)
-  # switch to cel.
+  # Switch to cel.
   mean_sst_c <- mean_sst - 273.15
   # crop depth
   depth_crop <- crop(depth, mean_sst_c)
-  # resample depth
+  # Resample depth
   depth_resample <- resample(depth_crop, mean_sst_c, method = "near")
-  # suitable sst matrix
+  # Suitable sst matrix
   animal_sst <- matrix(c(-Inf, minsst, 0,
                          minsst, maxsst, 1, 
                          maxsst, Inf, 0),
                        ncol = 3, byrow = TRUE)
-  # suitable depth matrix
+  # Suitable depth matrix
   animal_depth <- matrix(c(-Inf, mindepth, 0,
-                         mindepth, maxdepth, 1, 
-                         maxdepth, Inf, 0),
-                       ncol = 3, byrow = TRUE)
-  # reclassify
+                           mindepth, maxdepth, 1, 
+                           maxdepth, Inf, 0),
+                         ncol = 3, byrow = TRUE)
+  # Reclassify
   reclassified_sst <- classify(mean_sst_c, rcl = animal_sst)
   reclassified_depth <- classify(depth_resample, rcl = animal_depth)
+  reclassified_sst[reclassified_sst == 0] <- NA
+  reclassified_depth[reclassified_depth == 0] <- NA
   # rematch CRSs
   reclassified_depth <- project(reclassified_depth, reclassified_sst)
-  # multiple together to combine
+  # Multiple together to combine
   reclass_all <- reclassified_sst * reclassified_depth
-  # rasterize eez
+  # Rasterize eez
   wc_eez_rast <- rasterize(wc_eez, reclass_all, "rgn")
-  # crop reclassified data to eez
+  # Crop reclassified data to eez
   cropped_animal_locations <- crop(reclass_all, wc_eez_rast)
-  # mask animal locations to only eez
+  # Mask animal locations to only eez
   animal_eez <- mask(cropped_animal_locations, wc_eez_rast)
-  # find cell area of animal_eez
-  area_cell <- cellSize(animal_eez, unit = "km")
-  # find the answer :)
-  the_answer <- animal_eez * area_cell
-  # plot at the end with species name
-  zonal_answer <- zonal(the_answer, wc_eez_rast, "sum", na.rm = TRUE) 
-  zonal_answer 
+  # Find cell area of animal_eez
+  area_cell <- cellSize(animal_eez, 
+                        mask = TRUE,
+                        unit = "km")
+  # Multiply oyster data by area_cell data to find area of eez
+  eez_area <- animal_eez * area_cell
+  
+  # Use zonal function to separate area by EEZ
+  zonal_area <- zonal(eez_area, wc_eez_rast, "sum", na.rm = TRUE) 
+  
+  # Print table
+  kable(eez_suitable, digits = 2,
+        caption = "Suitable area in West Coast EEZs", 
+        col.names = c("EEZ Region", "Area (km^2)"))
+  
+  # Join for plotting
+  eez <- left_join(wc_eez, eez_suitable, by = "rgn")
+  
+  # Map suitable locations by EEZ
+  tm_shape(depth) +
+    tm_raster(palette = "-GnBu",
+              title = "Bathymetry\n(m above and below sea level)",
+              midpoint = 0,
+              legend.show = FALSE) +
+    tm_shape(eez, raster.downsample = TRUE) +
+    tm_polygons(col = "mean",
+                palette = "Purples",
+                alpha = 0.8,
+                linewidth = 0.2,
+                title = expression("Suitable habitat area (km"^2*")")) +
+    tm_text("rgn", size = 0.45) +
+    tm_compass(size = .5,
+               position = c("left", "bottom")) +
+    tm_scale_bar(position = c("left", "bottom")) +
+    tm_layout(legend.outside = TRUE,
+              frame = FALSE,
+              main.title = paste0("Suitable Habitats by West Coast EEZs\n(Exclusive Economic Zones) for ", species),
+              main.title.size = .7)
 }
-
-
-# maxsst, minsst, maxdepth, mindepth
-
-# oysters:
-#   sea surface temperature: 11-30°C
-# depth: 0-70 meters below sea level
-
-
-
-aqua_fun(30, 11, 0, -70)
-
-# red abalone: 
-#   sea surface temperature: 8°C - 18°C 
-# depth: 0-24 meters below sea level
-
-aqua_fun(18, 8, 0, -24)
-
